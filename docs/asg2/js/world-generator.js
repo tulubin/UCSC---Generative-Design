18e660023974dc8c49dd9254d772506551b15733
Lubin <tlb641226830@gmail.com>
2019-07-11 16:19:07 -0700

    update

--- docs/asg2/js/world-generator.js
+++ docs/asg2/js/world-generator.js
@@ -3,7 +3,7 @@ let WORLD_SIZE = 32;
 let WORLD_WIDTH = 32;
 let WORLD_HEIGHT = 32;
 let WORLD_DEPTH = 32;
-
+let GRADE = 0.025;
 class WorldGenerator {
     constructor(seed) {
         noiseSeed(seed);
@@ -16,39 +16,37 @@ class WorldGenerator {
         for (let i = 0; i < WORLD_SIZE; i++) {
             for (let j = 0; j < WORLD_SIZE; j++) {
 
-                let worldHeight = noise(i / 10, j / 10) * 1;
-                // let altitude = Math.floor(worldHeight * 10) + 1;
+                let altitude = noise(i * GRADE, j * GRADE) * 1;
+                // let altitude = Math.floor(altitude * 10) + 1;
 
                 // noStroke();
                 stroke(100, 100, 100);
                 noFill();
 
-                this.biome(worldHeight);
-                translate(0, -altitude * BLOCK_SIZE, 0);
+                if (altitude >= 0 && altitude < 0.3) { // deep sea
+                    texture(seaTexture);
+                }
+                else if (altitude >= 0.3 && altitude < 0.4) { // shalow water
+                    texture(waterTexture);
+                }
+                else if (altitude >= 0.4 && altitude < 0.5) { // sand
+                    texture(sandTexture);
+                }
+                else if (altitude >= 0.5 && altitude < 0.7) { // grass
+                    texture(grassTexture);
+                }
+                else if (altitude >= 0.7 && altitude <= 1) { // mountain
+                    texture(dirtTexture);
+                }
+
+                let blockAltitude = Math.floor(map(altitude, 0, 1, 0, WORLD_HEIGHT));
+                translate(0, -blockAltitude * BLOCK_SIZE, 0);
                 box(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-                translate(0, altitude * BLOCK_SIZE, 0);
+                translate(0, blockAltitude * BLOCK_SIZE, 0);
                 // ambientLight();
                 translate(0, 0, BLOCK_SIZE);
             }
             translate(-BLOCK_SIZE, 0, -(WORLD_SIZE * BLOCK_SIZE));
-        }
-    }
-
-    biome() {
-        if (altitude >= 0 && altitude < 3) { // deep sea
-            texture(seaTexture);
-        }
-        else if (altitude >= 3 && altitude < 4) { // shalow water
-            texture(waterTexture);
-        }
-        else if (altitude >= 4 && altitude < 5) { // sand
-            texture(sandTexture);
-        }
-        else if (altitude >= 5 && altitude < 7) { // grass
-            texture(grassTexture);
-        }
-        else if (altitude >= 7 && altitude < 10) { // mountain
-            texture(dirtTexture);
         }
     }
 }
