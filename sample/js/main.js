7a4447205d2ce671666aff79a36005b4a339c1dc
Lubin <tlb641226830@gmail.com>
2019-07-10 22:29:38 -0700

    update

---
+++ sample/js/main.js
@@ -0,0 +1,13 @@
+let myImg = document.getElementById("myImg");
+let clicked = true;
+myImg.onclick = function () {
+    if (clicked) {
+        let myName = document.getElementById("myName");
+        myName.innerHTML = "";
+        clicked = !clicked;
+    } else {
+        let myName = document.getElementById("myName");
+        myName.innerHTML = "Lubin Tu";
+        clicked = !clicked;
+    }
+}
