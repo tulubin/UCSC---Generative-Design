7b3ed427fa90759ef0c8ba0f9d8ff589e4c4f4d8
Lubin <tlb641226830@gmail.com>
2019-07-11 20:52:39 -0700

    update

--- docs/asg2/js/sketch.js
+++ docs/asg2/js/sketch.js
@@ -11,16 +11,17 @@ let waterTexture;
 let sandTexture;
 let grassTexture;
 let dirtTexture;
-let MOVE_SPEED = 20;
+let MOVE_SPEED = 80;
 let WORLD_SEED = 101010;
 let input, button, instructions;
-let BLOCK_SIZE = 16;
-let WORLD_SIZE = 32;
+let BLOCK_SIZE = 128;
+// let WORLD_SIZE = 32;
 let WORLD_WIDTH = 128;
 let WORLD_HEIGHT = 32;
 let WORLD_DEPTH = 128;
-let GRADE = 0.025;
-let VIEW_DIS = 30 * BLOCK_SIZE;
+let GRADE = 0.02;
+let VIEW_DIS = 32 * BLOCK_SIZE;
+let SEA_LEVEL = 12;
 
 function preload() {
     stoneTexture = loadImage('assets/stone.png');
@@ -51,23 +52,31 @@ function setup() {
     textSize(50);
 
     cam = createCamera();
-    cam.setPosition(0, -WORLD_HEIGHT * BLOCK_SIZE, 0);
-    // cam.pan(1.8);
-    // cam.tilt(0.9);
+    cam.setPosition(-WORLD_WIDTH / 2 * BLOCK_SIZE, -WORLD_HEIGHT * BLOCK_SIZE, WORLD_DEPTH / 2 * BLOCK_SIZE);
+    cam.pan(1.8);
+    cam.tilt(0.9);
 
     world = new WorldGenerator();
 
+
     // let vCube = createVector(100 - cam.eyeX, 100 - cam.eyeY, 100 - cam.eyeZ);
     // let vCam = createVector(cam.centerX - cam.eyeX, cam.centerY - cam.eyeY, cam.centerZ - cam.eyeZ);
+    // let vCam = createVector(cam.centerX - cam.eyeX, cam.centerY - cam.eyeY, cam.centerZ - cam.eyeZ);
+    // console.log(cam.centerX, cam.centerY, cam.centerZ);
+    // console.log(vCam);
 
-    // console.log(vCube);
-    
 }
 
 function draw() {
-    background(125);
+    background(255);
     updateCameraLocation()
     world.draw();
+    // translate(100, 100, 100);
+    // let vCube = createVector(100 - cam.eyeX, 100 - cam.eyeY, 100 - cam.eyeZ);
+    // let vCam = createVector(cam.centerX - cam.eyeX, cam.centerY - cam.eyeY, cam.centerZ - cam.eyeZ);
+    // console.log(degrees(vCam.angleBetween(vCube)));
+    // if (degrees(vCam.angleBetween(vCube)) < 25)
+    //     box(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 }
 
 function mouseDragged() {
@@ -91,10 +100,6 @@ function updateCameraLocation() {
     } else if (keyIsDown(70)) { // F to move down
         cam.move(0, MOVE_SPEED, 0);
     }
-    // let vCube = createVector(100 - cam.eyeX, 100 - cam.eyeY, 100 - cam.eyeZ);
-    // let vCam = createVector(cam.centerX - cam.eyeX, cam.centerY - cam.eyeY, cam.centerZ - cam.eyeZ);
-    // console.log(cam.centerX, cam.centerY, cam.centerZ);
-    // console.log(vCam);
 }
 
 
--- docs/asg2/js/world-generator.js
+++ docs/asg2/js/world-generator.js
@@ -11,43 +11,35 @@ class WorldGenerator {
             for (let j = 0; j < WORLD_DEPTH; j++) {
 
                 let altitude = noise(i * GRADE, j * GRADE) * 1;
-                // let altitude = Math.floor(altitude * 10) + 1;
                 let x = i * BLOCK_SIZE;
-                let y = Math.floor(map(altitude, 0, 1, 0, WORLD_HEIGHT)) * BLOCK_SIZE;
+                let y = Math.floor(map(altitude, 0, 1, 0, WORLD_HEIGHT - 1)) * BLOCK_SIZE + 1;
+                if (y < SEA_LEVEL * BLOCK_SIZE) {
+                    y = SEA_LEVEL * BLOCK_SIZE;
+                }
                 let z = j * BLOCK_SIZE;
-                // console.log(this.disOfCube(i, y, j));
                 let dis = this.disOfCube(-x, -y, z);
 
-                let vCube = createVector(x - cam.eyeX, y - cam.eyeY, z - cam.eyeZ);
+                let vCube = createVector(-x - cam.eyeX, -y - cam.eyeY, z - cam.eyeZ);
                 let vCam = createVector(cam.centerX - cam.eyeX, cam.centerY - cam.eyeY, cam.centerZ - cam.eyeZ);
 
-                // if (degrees(vCam.angleBetween(vCube)) < 25 && dis < VIEW_DIS) {
-                // console.log(degrees(vCam.angleBetween(vCube)));
-                if (dis < VIEW_DIS) {
+                if (degrees(vCam.angleBetween(vCube)) < 60 && dis < VIEW_DIS) {
                     // noStroke();
                     stroke(100, 100, 100);
                     noFill();
-
                     if (altitude >= 0 && altitude < 0.3) { // deep sea
                         texture(seaTexture);
-                    }
-                    else if (altitude >= 0.3 && altitude < 0.4) { // shalow water
+                    } else if (altitude > 0.3 && altitude <= 0.4) { // shalow water
                         texture(waterTexture);
-                    }
-                    else if (altitude >= 0.4 && altitude < 0.5) { // sand
+                    } else if (altitude > 0.4 && altitude <= 0.5) { // sand
                         texture(sandTexture);
-                    }
-                    else if (altitude >= 0.5 && altitude < 0.7) { // grass
+                    } else if (altitude > 0.5 && altitude <= 0.7) { // grass
                         texture(grassTexture);
-                    }
-                    else if (altitude >= 0.7 && altitude <= 1) { // mountain
+                    } else if (altitude > 0.7 && altitude <= 1) { // mountain
                         texture(dirtTexture);
                     }
-
                     translate(0, -y, 0);
                     box(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                     translate(0, y, 0);
-                    // ambientLight();
                 }
                 translate(0, 0, BLOCK_SIZE);
             }
@@ -64,14 +56,4 @@ class WorldGenerator {
         // console.log(dy);
         // return Math.sqrt(dy * dy);
     }
-
-    // fieldOfView(va, vb) {
-    //     // Store some information about them for below
-    //     let dot = va.dot(vb);
-    //     let la = va.length();
-    //     let lb = vb.length();
-
-    //     // Now to find the angle
-    //     return (Math.acos(dot / (la * lb)) * (180 / Math.PI));
-    // }
 }
