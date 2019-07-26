2a5f3f0bb406adc41aa3a7ffae580bcd6ff03b93
Lubin <tlb641226830@gmail.com>
2019-07-26 14:52:54 -0700

    update

--- docs/asg4/js/ga.js
+++ docs/asg4/js/ga.js
@@ -8,8 +8,11 @@ class Individual {
     }
 
     init() {
-        for(let i = 0; i < this.indSize; i++) {
-            this.gens[i] = int(random(2));
+        for (let i = 0; i < this.indSize; i++) {
+            console.log("random:");
+            // this.gens[i] = int(random(2));
+            this.gens[i] = randomFeatures();
+            console.log("random:", andomFeatures());
         }
     }
 }
@@ -25,7 +28,7 @@ class GeneticAlgorithm {
 
     init() {
         this.population = new Array(this.popSize);
-        for(let i = 0; i < this.popSize; i++) {
+        for (let i = 0; i < this.popSize; i++) {
             // Initialize individual i randomly
             this.population[i] = new Individual(this.indSize);
         }
@@ -45,7 +48,7 @@ class GeneticAlgorithm {
     }
 
     evaluate() {
-        for(let i = 0; i < this.popSize; i++) {
+        for (let i = 0; i < this.popSize; i++) {
             let individual = this.population[i];
             individual.fitness = this.fitFunc(individual.gens)
         }
@@ -55,7 +58,7 @@ class GeneticAlgorithm {
         let matingPool = new Array();
 
         // Select this.popSize Individual to be the parents
-        for(let i = 0; i < this.popSize; i++) {
+        for (let i = 0; i < this.popSize; i++) {
             let survivor = this.rouletteWheel();
             matingPool.push(survivor);
         }
@@ -70,7 +73,7 @@ class GeneticAlgorithm {
     reproduce(matingPool) {
         let newPopulation = new Array(this.popSize);
 
-        for(let i = 0; i < this.popSize; i++) {
+        for (let i = 0; i < this.popSize; i++) {
             let a = int(random(this.popSize));
             let b = int(random(this.popSize));
 
